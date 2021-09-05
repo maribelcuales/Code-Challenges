bacc6d3b174274f694e89a9a7cd5e255b715df0d
maribelcuales <maribelcuales@gmail.com>
2021-09-05 21:51:28 +0800

    Create new file JS Code Challenges to log submitted code challenges.

---
+++ JS-Code-Challenge.js
@@ -0,0 +1,9 @@
+// Create a Variable: var 
+var favoriteFood = 'pizza';
+console.log(favoriteFood);
+
+// Create a Variable: let
+let changeMe = true;
+console.log(changeMe);
+changeMe = false;
+console.log(changeMe);
