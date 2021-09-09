67e617ef44ee0f2c2ff448065e90d5fe57d9b38e
maribelcuales <maribelcuales@gmail.com>
2021-09-09 18:47:27 +0800

    Added logical operators.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -1,5 +1,5 @@
-// Create a Variable: var 
-var favoriteFood = 'pizza';
+// Create a Variable: var
+var favoriteFood = "pizza";
 console.log(favoriteFood);
 
 // Create a Variable: let
@@ -8,8 +8,8 @@ console.log(changeMe);
 changeMe = false;
 console.log(changeMe);
 
-// Create a Variable: const 
-const entree = 'Enchiladas';
+// Create a Variable: const
+const entree = "Enchiladas";
 console.log(entree);
 
 // Mathematical Assignment Operators
@@ -23,10 +23,10 @@ powerLevel -= 100;
 multiplyMe *= 11;
 quarterMe /= 4;
 
-console.log('The value of levelUp:', levelUp); 
-console.log('The value of powerLevel:', powerLevel); 
-console.log('The value of multiplyMe:', multiplyMe); 
-console.log('The value of quarterMe:', quarterMe);
+console.log("The value of levelUp:", levelUp);
+console.log("The value of powerLevel:", powerLevel);
+console.log("The value of multiplyMe:", multiplyMe);
+console.log("The value of quarterMe:", quarterMe);
 
 // The Increment and Decrement Operator
 let gainedDollar = 3;
@@ -39,25 +39,25 @@ lostDollar--;
 console.log(lostDollar);
 
 // String Concatenation with Variables
-let favoriteAnimal = 'dog';
-console.log('My favorite animal:' + favoriteAnimal);
+let favoriteAnimal = "dog";
+console.log("My favorite animal:" + favoriteAnimal);
 
 // String Interpolation using Template Literals
-const myName = 'Belle';
-const myCity = 'Paris';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+const myName = "Belle";
+const myCity = "Paris";
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
 
 // typeof operator: check the data type of a variable’s value
-let newVariable = 'Playing around with typeof.';
+let newVariable = "Playing around with typeof.";
 console.log(typeof newVariable);
 newVariable = 1;
 console.log(typeof newVariable);
 
-// If Statement 
+// If Statement
 let sale = true;
 sale = false;
 if (sale) {
-  console.log('Time to buy!');
+  console.log("Time to buy!");
 }
 
 // If...Else Statements - allow us to automate solutions to yes-or-no questions, also known as binary decisions.
@@ -66,16 +66,26 @@ let sale = true;
 sale = false;
 
 if (sale) {
-  console.log('Time to buy!');
+  console.log("Time to buy!");
 } else {
-  console.log('Time to wait for a sale.');
+  console.log("Time to wait for a sale.");
 }
 
 // Comparison Operators (>, <, <=,>=,===,!==)
 let hungerLevel = 7;
 
 if (hungerLevel > 7) {
-  console.log('Time to eat!');
+  console.log("Time to eat!");
 } else {
-  console.log('We can eat later!');
+  console.log("We can eat later!");
+}
+
+// Logical Operators - and(&&), or(||), not or bang(!)
+let mood = "sleepy";
+let tirednessLevel = 6;
+
+if (mood === "sleepy" && tirednessLevel > 8) {
+  console.log("time to sleep");
+} else {
+  console.log("not bed time yet");
 }
