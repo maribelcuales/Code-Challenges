655d04cc1f08c3db2b826617159443df4e43b1db
maribelcuales <maribelcuales@gmail.com>
2021-09-10 18:26:49 +0800

    Used circuit evaluation in assigning truthy or falsy values to variables.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -107,3 +107,10 @@ if (favoritePhrase) {
 } else {
   console.log('This string is definitely empty.');
 }
+
+// Truthy and Falsy Assignment 
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let tool = 'marker';
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
