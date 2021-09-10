821354064d9183a0b759f9c8aad758ea5b2c8d8b
maribelcuales <maribelcuales@gmail.com>
2021-09-10 21:34:32 +0800

    Add swtich statement.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -163,3 +163,20 @@ if (season === 'spring') {
   console.log('Invalid season.');
 }
 
+// The switch statement
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place' :
+    console.log('You get the gold medal!');
+    break;
+  case 'second place' :
+    console.log('You get the silver medal!');
+    break;
+  case 'third place' :
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
