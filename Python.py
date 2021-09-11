c4cd76611d7a14fdfc490e90972fa524cde617bc
maribelcuales <maribelcuales@gmail.com>
2021-09-11 15:43:06 +0800

    Added how to print variables.

--- Python.py
+++ Python.py
@@ -1,7 +1,26 @@
 # This is how to write a comment 
 
 # The print() function === console.log in JS
+# print a string 
 print("Hello World!")
 
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Chicken Cordon Blue"
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Rib Eye Steak"
+# Printing out dinner
+print("Dinner:")
+print(meal)
 
 
