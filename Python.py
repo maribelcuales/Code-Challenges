33801761a3cbe4880f6a3a4d137c5fce8967ff79
maribelcuales <maribelcuales@gmail.com>
2021-09-12 12:52:21 +0800

    Added exponents.

--- Python.py
+++ Python.py
@@ -56,3 +56,14 @@ quilt_length = 8
 print(quilt_width * quilt_length )
 
 
+# Exponents => use **
+# Calculation of squares for:
+# 6x6 quilt => prints "36"
+print(6 ** 2)
+# 7x7 quilt => prints "49"
+print(7 ** 2)
+# 8x8 quilt => prints "64"
+print(8 ** 2)
+
+# How many squares for 6 people to have 6 quilts each that are 6x6? => prints "1296"
+print(6 ** 2 * 6 * 6)
