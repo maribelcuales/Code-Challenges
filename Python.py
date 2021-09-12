79d16830885dba93296a802a5be25185b077e45f
maribelcuales <maribelcuales@gmail.com>
2021-09-12 14:50:48 +0800

    Added string concatenation.

--- Python.py
+++ Python.py
@@ -78,3 +78,17 @@ print(my_team)
 my_team = 28 % 4
 # Prints "0"
 print(my_team)
+
+
+# String Concatenation
+# str() => converts variables to strings
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6 
+print(message)
