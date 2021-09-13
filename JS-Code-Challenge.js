5da9a7f2dddfaded07f1c336aae85f41ffddb89c
maribelcuales <maribelcuales@gmail.com>
2021-09-13 11:46:42 +0800

    Added arrow functions.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -180,3 +180,29 @@ switch (athleteFinalPosition) {
     console.log('No medal awarded.');
     break;
 }
+
+
+/* ========== FUNCTIONS ========== */
+
+// Arrow function with two arguments 
+const sum = (firstParam, secondParam) => { 
+  return firstParam + secondParam; 
+}; 
+console.log(sum(2,5)); // Prints: 7 
+ 
+// Arrow function with no arguments 
+const printHello = () => { 
+  console.log('hello'); 
+}; 
+printHello(); // Prints: hello
+ 
+// Arrow functions with a single argument 
+const checkWeight = weight => { 
+  console.log(`Baggage weight : ${weight} kilograms.`); 
+}; 
+checkWeight(25); // Prints: Baggage weight : 25 kilograms.
+ 
+ 
+// Concise arrow functions
+const multiply = (a, b) => a * b; 
+console.log(multiply(2, 30)); // Prints: 60
