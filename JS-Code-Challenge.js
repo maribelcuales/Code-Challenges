43ffa2cd8486fee3cc25320de743b1adf7edbd51
maribelcuales <maribelcuales@gmail.com>
2021-09-14 19:28:15 +0800

    Added reverse loop.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -239,4 +239,15 @@ while (i < 5) {
   i++;
 }
 
+// reverese loop
+const items = ['apricot', 'banana', 'cherry'];
+ 
+for (let i = items.length - 1; i >= 0; i -= 1) {
+  console.log(`${i}. ${items[i]}`);
+}
+
+// Prints: 2. cherry
+// Prints: 1. banana
+// Prints: 0. apricot
+
 
