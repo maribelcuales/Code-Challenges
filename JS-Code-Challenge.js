b0f983ed7e3cb0e9c95a72df62d4f5809d3c3dfe
maribelcuales <maribelcuales@gmail.com>
2021-09-14 19:35:05 +0800

    Added break keyword and a nested for loop.

--- JS-Code-Challenge.js
+++ JS-Code-Challenge.js
@@ -276,3 +276,29 @@ for (let i = 0; i < array.length; i++){
 // Output: Every item in the array
 
 
+// break keyword
+for (let i = 0; i < 99; i += 1) {
+  if (i > 5) {
+     break;
+  }
+  console.log(i)
+}
+// Output: 0 1 2 3 4 5
+
+
+// Nested For Loop
+for (let outer = 0; outer < 2; outer += 1) {
+  for (let inner = 0; inner < 3; inner += 1) {
+    console.log(`${outer}-${inner}`);
+  }
+}
+ 
+/* 
+Output:
+0-0
+0-1
+0-2
+1-0
+1-1
+1-2
+*/
