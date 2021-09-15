581ecc2a3062f60bbeb2375c29c11bd2ebca24df
maribelcuales <maribelcuales@gmail.com>
2021-09-15 21:28:09 +0800

    Python solution added to day 1: Data Types.

--- 30-days-code-challenge/python-solution.py
+++ 30-days-code-challenge/python-solution.py
@@ -1,4 +1,4 @@
-# Day 0: Hello, World
+# ///////  Day 0: Hello, World  ///////
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 input_string = input()
 
@@ -7,3 +7,34 @@ print('Hello, World.')
 
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
 print(input_string)
+
+
+# ///////  Day 1: Data Types  ///////
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = None
+b = None
+c = None
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input())
+
+# a = 12
+# b = 4.0
+# c = 'is the best place to learn and practice coding!'
+
+# Print the sum of both integer variables on a new line.
+print(i + a)
+
+# Print the sum of the double variables on a new line.
+print(d + b)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + c)
+
+
+
