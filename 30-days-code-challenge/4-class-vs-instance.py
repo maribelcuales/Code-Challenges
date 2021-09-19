92f3c6d78f057c1173fecec7a73b76395947dab7
maribelcuales <maribelcuales@gmail.com>
2021-09-19 19:17:29 +0800

    Added solution to Day 4 Code Challenge.

--- 30-days-code-challenge/4-class-vs-instance.py
+++ 30-days-code-challenge/4-class-vs-instance.py
@@ -3,16 +3,35 @@
 # write a Person with an instance variable age, and a constructor that takes an integer, initialAge, as a parameter 
 # constructor must assign initialAge to age if argument passed is not negative
 # if initialAge parameter is negative, set age = 0 and print 'Age is not valid' 
-# 
 
+# yearPasses() should increase the  instance variable by .
+# amIOld() should perform the following conditional actions:
+## If age < 13, print 'You are young.'
+## If age > 13 and < 18, print 'You are a teenager.'
+## Otherwise, print You are old..
 
 class Person:
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
+        if initialAge > 0: 
+          self.initialAge = initialAge
+        else:
+          self.initialAge = 0
+          print('Age is not valid, setting age to 0.')
+
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        if self.initialAge < 13:
+          print('You are young.')
+        elif self.initialAge < 18:
+          print('You are a teenager.')
+        else:
+          print('You are old.') 
+
     def yearPasses(self):
         # Increment the age of the person in here
+        self.initialAge += 1
+
 
 t = int(input())
 for i in range(0, t):
