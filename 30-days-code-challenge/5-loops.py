3327b6026b75513e8bf9f49c30b3c5e88deee5bc
maribelcuales <maribelcuales@gmail.com>
2021-09-23 21:06:48 +0800

    Solution added to Day 5 code challenge.

--- 30-days-code-challenge/5-loops.py
+++ 30-days-code-challenge/5-loops.py
@@ -1,12 +1,25 @@
 #!/bin/python3
 
+# given an integer 
+# print its first 10 multiples, each multiple n x i (where 1 < i < 10) 
+# each multiple be printed on a new line: n x i = result 
+
 import math
 import os
 import random
 import re
 import sys
 
+n = int(input())
+i = 1 
+
+while i < 11:
+  result = n * i
+  multipleResult = '{} x {} = {}'
+  print(multipleResult.format(n, i, result))
+  i += 1 
+  
 
 
-if __name__ == '__main__':
-    n = int(input().strip())
+#if __name__ == '__main__':
+#    n = int(input().strip())
