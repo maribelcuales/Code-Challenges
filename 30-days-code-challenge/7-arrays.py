220ec0784ad5db014a0843f1cbd62eeb8df954b1
maribelcuales <maribelcuales@gmail.com>
2021-09-30 20:52:53 +0800

    Added day 7 code challenge solution.

--- 30-days-code-challenge/7-arrays.py
+++ 30-days-code-challenge/7-arrays.py
@@ -6,9 +6,19 @@ import random
 import re
 import sys
 
+N=int(input())
+
+A = str(input()).split(" ")
+A.reverse()
+
+for num in A:
+    print(num + " ", end="")
 
 
+
+"""
 if __name__ == '__main__':
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
+"""
